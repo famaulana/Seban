4772b7f05a6820d1e6964e67f9aa4a380b258184
aldimakarim <aldimakarimhaha@gmail.com>
2019-07-25 14:31:26 +0700

    +MapsActivity and update color +Add Logo

---
+++ app/src/main/java/com/android/serban/MapsActivity.kt
@@ -0,0 +1,12 @@
+package com.android.serban
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.maps_seban_activity)
+    }
+}
