1a62d20d40734100a6a46c5d3acd3f4a62cdfee5
Farhan Agung Maulana <famagung@gmail.com>
2019-08-14 15:38:27 +0700

    +Fix Layout

--- app/src/main/java/com/android/serban/Fragment/HomeFragment.kt
+++ app/src/main/java/com/android/serban/Fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.android.serban.EngineService
 import com.android.serban.MapsActivity
@@ -12,21 +13,26 @@ import com.android.serban.PrefHelper
 import com.android.serban.R
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.home.*
-import kotlinx.android.synthetic.main.profile.*
 
 class HomeFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.home, container, false)
     }
+
     lateinit var fAuth: FirebaseAuth
     lateinit var preferences: PrefHelper
     lateinit var storageReference: StorageReference
     lateinit var firebaseStorage: FirebaseStorage
+    var uidCheck = "btXiRU8170YrlgYHdjQiXaZWz4k1"
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         preferences = PrefHelper(context!!)
@@ -36,7 +42,7 @@ class HomeFragment : Fragment() {
         FirebaseDatabase.getInstance().getReference("user/${fAuth.uid}")
             .child("username").addListenerForSingleValueEvent(object : ValueEventListener {
                 override fun onDataChange(p0: DataSnapshot) {
-                    tv_name2.setText(p0.value.toString())
+                    tv_name2.text = p0.value.toString()
                 }
                 override fun onCancelled(p0: DatabaseError) {
                 }
@@ -49,6 +55,18 @@ class HomeFragment : Fragment() {
                         .error(R.drawable.ic_launcher_background)
                         .into(ava2)
                 }
+                override fun onCancelled(p0: DatabaseError) {
+                }
+            })
+        FirebaseDatabase.getInstance().getReference("user/${fAuth.uid}")
+            .child("id").addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(p0: DataSnapshot) {
+                    val uid = p0.value.toString()
+                    if (uid == uidCheck) {
+                        ll_find_order_home.isVisible = true
+                    }
+                }
+
                 override fun onCancelled(p0: DatabaseError) {
                 }
             })
